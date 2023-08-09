3f0edb6402feae288235a33b04243ca7e323cf26
Brock Williams <b.williams54255@gmail.com>
2023-08-09 16:48:34 -0700

    Added two functions in the javascript file named getPasswordLength and generatepassword. This fixed the syntax error caused by the writepassword function. Returns undefined.

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,30 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+function getPasswordLength () {
+    var passwordLength = prompt("Enter your desired password length (8-128 characters).");
+    //ParseInt converts the prompt value into an integer and if it's not will return NaN 
+    return parseInt(passwordLength);
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+    var speicalChars = "!@#$%^&*()_-+=<>?";
+    var numericChars = "0123456789";
+    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  }
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
+
