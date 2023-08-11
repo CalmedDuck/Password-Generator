e13c18ff35b1cfcd07cc550e5ed3178e3c34830e
Brock Williams <b.williams54255@gmail.com>
2023-08-10 22:04:52 -0700

    fixed a bug where the generator would not use lower case characters.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Password-Generator
+
+Random password generator.
+
+## Description
+
+This site will generate a random password depending on how long and what type of characters you want to include.
+
+## Installation
+
+This website can be acessed through the following link. 
+
+
+
+## Usage
+
+You can use the nav section and click on the links to take you to the following section of the page.
+
+![Alt Text]()
+
+## Credits
+
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,53 @@
 var generateBtn = document.querySelector("#generate");
 
-function getPasswordLength () {
-    var passwordLength = prompt("Enter your desired password length (8-128 characters).");
-    //ParseInt converts the prompt value into an integer and if it's not will return NaN 
-    return parseInt(passwordLength);
-
+function getPasswordLength() {
+  var passwordLength = prompt("Enter your desired password length (8-128 characters).");
+  return parseInt(passwordLength);
 }
-//asking the user what characters they want to include in their password.
-function desiredCharacterTypes () {
-    var includeLowercase = confirm("Include lowercase letters? (Ok/Cancel)");
-    var includeUppercase =confirm("Include Upper Case letters? (Ok/Cancel)");
-    var includeNumeric = confirm("Include numeric characters? (Ok/Cancel)");
-    var includeSpecial = confirm("Include special characters? (Ok/Cancel)");
 
-    return {
-        includeLowercase: includeLowercase,
-        includeUppercase: includeUppercase,
-        includeNumeric: includeNumeric,
-        includeSpeical: includeSpecial
-    };
-  
-  //all of the characters being used
-  }
+function desiredCharacterTypes() {
+  var includeLowercase = confirm("Include lowercase letters? (Ok/Cancel)");
+  var includeUppercase = confirm("Include Upper Case letters? (Ok/Cancel)");
+  var includeNumeric = confirm("Include numeric characters? (Ok/Cancel)");
+  var includeSpecial = confirm("Include special characters? (Ok/Cancel)");
+
+  return {
+    includeLowercase: includeLowercase,
+    includeUppercase: includeUppercase,
+    includeNumeric: includeNumeric,
+    includeSpecial: includeSpecial // Corrected typo here
+  };
+}
+
 function generatePassword() {
-    var speicalChars = "!@#$%^&*()_-+=<>?";
-    var numericChars = "0123456789";
-    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-//requirements for the password length
-    var passwordLength = getPasswordLength();
-    if (isNaN(passwordLength)) {
-        alert('Password length must be between 8-128 characters.');
-        return '';
-    }
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var specialChars = "!@#$%^&*()_-+=<>?";
+  var numericChars = "0123456789";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    if (passwordLength < 8) {
-        alert("Password length must be at least 8 characters.");
-        return '';
-    }
+  var passwordLength = getPasswordLength();
+  if (isNaN(passwordLength)) {
+    alert('Password length must be between 8-128 characters.');
+    return '';
+  }
 
-    if (passwordLength > 128) {
-        alert("Password length may be no more than 128 characters.");
-        return '';
+  if (passwordLength < 8) {
+    alert("Password length must be at least 8 characters.");
+    return '';
+  }
 
-    }
+  if (passwordLength > 128) {
+    alert("Password length may be no more than 128 characters.");
+    return '';
+  }
 
-    var characterTypes = desiredCharacterTypes();
-    var characterPool = '';
+  var characterTypes = desiredCharacterTypes();
+  var characterPool = '';
 
-    if (characterTypes.includeLowercase) {
-        characterPool += uppercaseChars;
-    }
+  if (characterTypes.includeLowercase) {
+    characterPool += lowercaseChars;
+  }
 
-    
   if (characterTypes.includeUppercase) {
     characterPool += uppercaseChars;
   }
@@ -69,7 +64,7 @@ function generatePassword() {
     alert("At least one character type must be selected.");
     return '';
   }
-//Chooses a random character from the appropriate pools and repeats for the length of the requested password.
+
   var generatedPassword = '';
   for (var i = 0; i < passwordLength; i++) {
     generatedPassword += characterPool[Math.floor(Math.random() * characterPool.length)];
@@ -77,20 +72,12 @@ function generatePassword() {
 
   return generatedPassword;
 }
-  
 
-// Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-  
-  
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+  passwordText.value = password;
+}
 
-
+generateBtn.addEventListener("click", writePassword);
